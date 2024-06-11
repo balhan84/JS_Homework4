1f2aae533ad2d053a0e5cf53e583c60998efab94
balhan84 <balamutenkoanna@gmail.com>
2024-06-11 19:30:08 +0300

    Fix - even numbers

--- assets/js/array.js
+++ assets/js/array.js
@@ -1,39 +1,39 @@
-//task#1
-//Створити числовий масив та проініціалізувати його
-// const numbers = [];
-// const amountNumbers = 7;
-// for (let i = 0; i < amountNumbers; i++) {
-//   numbers.push(Math.floor(Math.random() * 10));
-// }
-// console.log(numbers);
-//Видалити останній і початковий елемент з масиву, додати елемент до початку і кінця.
-// const lastEl = numbers.pop();
-// const firstEl = numbers.shift();
-// numbers.push(25);
-// numbers.unshift(11);
+// task#1
+// Створити числовий масив та проініціалізувати його
+const numbers = [];
+const amountNumbers = 7;
+for (let i = 0; i < amountNumbers; i++) {
+  numbers.push(Math.floor(Math.random() * 10));
+}
+console.log(numbers);
+// Видалити останній і початковий елемент з масиву, додати елемент до початку і кінця.
+const lastEl = numbers.pop();
+const firstEl = numbers.shift();
+numbers.push(25);
+numbers.unshift(11);
 
-// //Вивести розмір масиву.
-// console.log(numbers.length);
-// //Зробити копію масиву.
-// const numbers2 = [...numbers];
-//Вивести елементи з парними індексами.
-// for (const index in numbers) {
-//   if (index % 2 === 0 && index > 0) {
-//     console.log(index);
-//   }
-// }
-//Знайти добуток елементів масиву.
+//Вивести розмір масиву.
+console.log(numbers.length);
+//Зробити копію масиву.
+const numbers2 = [...numbers];
+// Вивести елементи з парними індексами.
+for (const index in numbers) {
+  if (index % 2 === 0 && index >= 0) {
+    console.log(index);
+  }
+}
+// Знайти добуток елементів масиву.
 
-// function multArray(arr) {
-//   let mult = 1;
-//   for (let i = 0; i < arr.length; i++) {
-//     mult *= arr[i];
-//   }
-//   return mult;
-// }
-// console.log(multArray(numbers));
+function multArray(arr) {
+  let mult = 1;
+  for (let i = 0; i < arr.length; i++) {
+    mult *= arr[i];
+  }
+  return mult;
+}
+console.log(multArray(numbers));
 
-//Задано масив з описом телефонів з полями id, brand, model, color, price, RAM
+// Задано масив з описом телефонів з полями id, brand, model, color, price, RAM
 
 // const phones = [
 //   {
@@ -117,7 +117,7 @@
 //     RAM: "16GB",
 //   },
 // ];
-//Сформувати розмітку для карток.
+// // Сформувати розмітку для карток.
 // for (let i = 0; i < phones.length; i++) {
 //   document.write(`
 // <article>
@@ -129,7 +129,7 @@
 // </article>
 // `);
 // }
-//Знайти середню ціну телефонів.
+// // Знайти середню ціну телефонів.
 
 // function calcAveragePrice(pricePhones) {
 //   let sum = 0;
@@ -140,9 +140,9 @@
 // }
 // console.log(calcAveragePrice(phones));
 
-// Отримати новий масив із заданого, який міститиме лише ненульові числа (-1, 5, 0, 9, -10 => -1, 5, 9, -10)
+// // Отримати новий масив із заданого, який міститиме лише ненульові числа (-1, 5, 0, 9, -10 => -1, 5, 9, -10)
 
-// const numbers = [-1, 5, 0, 9, -10];
+// // const numbers = [-1, 5, 0, 9, -10];
 
 // const notnullNumbers = numbers.filter(newArrNumbers);
 // function newArrNumbers(n) {
@@ -150,7 +150,7 @@
 // }
 // console.log(notnullNumbers);
 
-//Отримати новий масив їх заданого, який міститиме всі елементи вихідного, поділені на 100 (99, 5, 0, 9, 30 => 0.99, 0.05, 0, 0.09, 0.3). // map
+// Отримати новий масив їх заданого, який міститиме всі елементи вихідного, поділені на 100 (99, 5, 0, 9, 30 => 0.99, 0.05, 0, 0.09, 0.3). // map
 
 // const numbers = [99, 5, 0, 9, 30];
 // const divideNumbers = numbers.map((numbers) => ({
@@ -159,17 +159,17 @@
 // }));
 // console.log(divideNumbers);
 
-//Вивести елементи масиву, зведені у куб. // forEach
+// // Вивести елементи масиву, зведені у куб. // forEach
 
-// const numbers = [3, 5, 2, 1, 4];
+// // const numbers = [3, 5, 2, 1, 4];
 
 // numbers.forEach((num) => {
 //   let cube = num ** 3;
 //   console.log(cube);
 // });
 
-//Визначити індекс елемента, квадрат якого дорівнює 100, і видалити його,
-//або видати діагностичне повідомлення, якщо такого елементу не існує. // findIndex
+// Визначити індекс елемента, квадрат якого дорівнює 100, і видалити його,
+// або видати діагностичне повідомлення, якщо такого елементу не існує. // findIndex
 
 // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
@@ -183,10 +183,10 @@
 // numbers.splice(findSqrIndex, 1);
 // console.log(numbers);
 
-//*Перевірити, чи всі елементи масиву є парними числами (* або простими числами). // every
+// // *Перевірити, чи всі елементи масиву є парними числами (* або простими числами). // every
 // const evenNumbers = numbers.every((num) => num % 2 === 0);
 // console.log(evenNumbers);
 
-//Перевірити, чи є у масиві бодай один від'ємний елемент. // some
+// // Перевірити, чи є у масиві бодай один від'ємний елемент. // some
 // const negNumbers = numbers.some((num) => num < 0);
 // console.log(negNumbers);
